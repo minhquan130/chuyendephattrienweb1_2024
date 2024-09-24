c25190965c29df47e9f56bb2d572d866668b436e
minhq130 <minhq130@gmail.com>
2024-09-24 15:21:44 +0700

    update

--- type_hint/demo.php
+++ type_hint/demo.php
@@ -9,102 +9,102 @@ include("./B.php");
 class Demo{
     public function typeAReturnA() : A {
         echo __FUNCTION__."</br>";
-        return new A;
+        return new A();
     }
 
     public function typeBReturnA() : B {
         echo __FUNCTION__."</br>";
-        return new A;
+        return new A();
     }
 
     public function typeCReturnA() : C {
         echo __FUNCTION__."</br>";
-        return new A;
+        return new A();
     }
 
     public function typeIReturnA() : I {
         echo __FUNCTION__."</br>";
-        return new A;
+        return new A();
     }
 
     public function typeNullReturnA() : null {
         echo __FUNCTION__."</br>";
-        return new A;
+        return new A();
     }
 
     public function typeAReturnB() : A {
         echo __FUNCTION__."</br>";
-        return new B;
+        return new B();
     }
 
     public function typeBReturnB() : B {
         echo __FUNCTION__."</br>";
-        return new B;
+        return new B();
     }
 
     public function typeCReturnB() : C {
         echo __FUNCTION__."</br>";
-        return new B;
+        return new B();
     }
 
     public function typeIReturnB() : I {
         echo __FUNCTION__."</br>";
-        return new B;
+        return new B();
     }
 
     public function typeNullReturnB() : null {
         echo __FUNCTION__."</br>";
-        return new B;
+        return new B();
     }
 
     public function typeAReturnC() : A {
         echo __FUNCTION__."</br>";
-        return new C;
+        return new C();
     }
 
     public function typeBReturnC() : B {
         echo __FUNCTION__."</br>";
-        return new C;
+        return new C();
     }
 
     public function typeCReturnC() : C {
         echo __FUNCTION__."</br>";
-        return new C;
+        return new C();
     }
 
     public function typeIReturnC() : I {
         echo __FUNCTION__."</br>";
-        return new C;
+        return new C();
     }
 
     public function typeNullReturnC() : null {
         echo __FUNCTION__."</br>";
-        return new C;
+        return new C();
     }
 
     public function typeAReturnI() : A {
         echo __FUNCTION__."</br>";
-        return new I;
+        return new I();
     }
 
     public function typeBReturnI() : B {
         echo __FUNCTION__."</br>";
-        return new I;
+        return new I();
     }
 
     public function typeCReturnI() : C {
         echo __FUNCTION__."</br>";
-        return new I;
+        return new I();
     }
 
     public function typeIReturnI() : I {
         echo __FUNCTION__."</br>";
-        return new I;
+        return new I();
     }
 
     public function typeNullReturnI() : null {
         echo __FUNCTION__."</br>";
-        return new I;
+        return new I();
     }
 
     public function typeAReturnNull() : A {
